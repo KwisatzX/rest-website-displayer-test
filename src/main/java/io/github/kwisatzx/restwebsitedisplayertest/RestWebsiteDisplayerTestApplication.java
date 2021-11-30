52cfd7c2ac233b99e8df4460d9e79bb270ca74ad
KwisatzX <kwisatz4@gmail.com>
2021-11-30 16:58:00 +0100

    Initial commit

---
+++ src/main/java/io/github/kwisatzx/restwebsitedisplayertest/RestWebsiteDisplayerTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.kwisatzx.restwebsitedisplayertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWebsiteDisplayerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestWebsiteDisplayerTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/kwisatzx/restwebsitedisplayertest/RestWebsiteDisplayerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kwisatzx.restwebsitedisplayertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWebsiteDisplayerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
